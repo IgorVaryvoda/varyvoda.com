0e1feda01314cdae74d10179ef28ed603a803f1b
Igor Varyvoda <igor@varyvoda.com>
2019-03-13 22:31:32 +0100

    too late

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -47,8 +47,6 @@ export const Sidebar = (props) => (
                 telegram
                 github
                 email
-                rss
-                vkontakte
               }
             }
           }
