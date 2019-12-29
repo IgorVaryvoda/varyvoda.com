2bd810ef540aceb88b7a9b463faf3d5da58e3c6d
igorvaryvoda <igor@varyvoda.com>
2019-12-29 20:38:46 +0100

    sidebar contact...2

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -16,11 +16,8 @@ const Menu = ({ menu }) => (
           </Link>
         </li>
       ))}
-      <li>
-      <Link
-        to={'https://docs.google.com/forms/d/e/1FAIpQLSd62z2gG7dCch_WAadc9G3Zic1HyWhGzxyTLp3pUE8SxylhHA/viewform'}
-        className={styles['menu__list-item-link']}
-        >Contact</Link>
+      <li class="menu__list-item">
+      <a class="menu__list-item-link" href="https://docs.google.com/forms/d/e/1FAIpQLSd62z2gG7dCch_WAadc9G3Zic1HyWhGzxyTLp3pUE8SxylhHA/viewform">Contact</a>
       </li>
     </ul>
   </nav>
